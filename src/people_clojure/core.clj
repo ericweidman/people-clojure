aadac619b6ede96096f9e459c21e0b910d003962
ericweidman <ericweidman@gmail.com>
2016-03-23 10:46:15 -0400

    Added web stuff

--- src/people_clojure/core.clj
+++ src/people_clojure/core.clj
@@ -1,13 +1,16 @@
 (ns people-clojure.core
   (:require [clojure.string :as str]
-            [clojure.walk :as walk])
+            [clojure.walk :as walk]
+            [compojure.core :as c]
+            [ring.adapter.jetty :as j]) 
+             
   (:gen-class))
 
-(defn -main []
+(defn read-people []
   (let [people (slurp "people.csv")
         people (str/split-lines people)
         people (map (fn [line]
-                      (str/split line #","))
+                      (str/split #","))
                     people)
         header (first people)
         people (rest people)
@@ -18,5 +21,11 @@
         people (filter (fn [line]
                          (= (:country line) "Brazil"))
                        people)]
-    (spit "filtered_people.edn" (pr-str people))
+    ;(spit "filtered_people.edn" (pr-str people))
     people))
+(c/defroutes app
+  (c/GET "/" request
+    "Clojure, motherucker. ()()()(())))))))))))))))))))))))))))))))))))"))
+
+(defn -main []
+  (j/run-jetty app {:port 3000}))
