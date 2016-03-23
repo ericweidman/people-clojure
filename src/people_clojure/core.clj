2f739b4b899b4b271104bde028af01b109ff834a
ericweidman <ericweidman@gmail.com>
2016-03-23 12:03:29 -0400

    Added filtering

--- src/people_clojure/core.clj
+++ src/people_clojure/core.clj
@@ -2,30 +2,58 @@
   (:require [clojure.string :as str]
             [clojure.walk :as walk]
             [compojure.core :as c]
-            [ring.adapter.jetty :as j]) 
-             
+            [ring.adapter.jetty :as j] 
+            [ring.middleware.params :as p]
+            [hiccup.core :as h])
   (:gen-class))
 
 (defn read-people []
   (let [people (slurp "people.csv")
         people (str/split-lines people)
         people (map (fn [line]
-                      (str/split #","))
+                      (str/split line #","))
                     people)
         header (first people)
         people (rest people)
         people (map (fn [line]
                       (apply hash-map (interleave header line)))
                   people)
-        people (walk/keywordize-keys people)
-        people (filter (fn [line]
-                         (= (:country line) "Brazil"))
-                       people)]
+        people (walk/keywordize-keys people)]
     ;(spit "filtered_people.edn" (pr-str people))
     people))
+
+(defn countries-html [people]
+  (let [all-countries (map :country people)
+        unique-countries (set all-countries)
+        sorted-countries (sort unique-countries)]
+    [:div
+     (map (fn [country]
+            [:span
+             [:a {:href (str "/?country=" country)} country]
+             " "])
+         sorted-countries)]))
+           
+
+(defn people-html [people]
+  [:ol
+   (map (fn [person]
+          [:li (str (:first_name person) " " (:last_name person))])
+        people)])
+
+
 (c/defroutes app
   (c/GET "/" request
-    "Hello, world!"))
+    (let [params (:params request)
+          country (get params "country")
+          country (or country "Brazil")
+          people (read-people)
+          filtered-people (filter (fn [person]
+                                    (= (:country person) country))
+                            people)]
+     (h/html [:html
+              [:body 
+               (countries-html people)
+               (people-html filtered-people)]]))))
 
 (defn -main []
-  (j/run-jetty app {:port 3000}))
+  (j/run-jetty (p/wrap-params app) {:port 3000}))
